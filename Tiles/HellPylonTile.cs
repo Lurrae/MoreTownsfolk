5abf881dcbfa2fcf907b95593712f1319065036a
Tepig125 <ar.murphy@live.com>
2024-02-02 15:32:02 -0600

    Add drawcode to utilize the Underworld Pylon's new glowmask

--- Tiles/HellPylonTile.cs
+++ Tiles/HellPylonTile.cs
@@ -13,5 +13,16 @@ namespace MoreTownsfolk.Tiles
 		{
 			return pylonInfo.PositionInTiles.Y >= Main.UnderworldLayer;
 		}
+
+		public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+		{
+			Texture2D tex = Request<Texture2D>(Texture + "_Glow").Value;
+			Tile tile = Main.tile[i, j];
+			Vector2 zero = new(Main.offScreenRange, Main.offScreenRange);
+			if (Main.drawToScreen)
+				zero = Vector2.Zero;
+
+			spriteBatch.Draw(tex, new Vector2(i * 16, j * 16) - Main.screenPosition + zero, new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, 16), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+		}
 	}
 }
